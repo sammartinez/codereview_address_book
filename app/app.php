858fc41492aa0d199112e168465fc1d519b8ec24
Sam Martinez <sammartinez19@gmail.com>
2015-08-08 07:01:52 -0700

    Changing spacing back to 4 spaces and also changing a couple of variables

--- app/app.php
+++ app/app.php
@@ -1,41 +1,41 @@
 <?php
-        require_once __DIR__."/../vendor/autoload.php";
-        require_once __DIR__."/../src/Contact.php";
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Contact.php";
 
-        //Session - Stores Cookies
-        session_start();
+    //Session - Stores Cookies
+    session_start();
 
-        if(empty($_SESSION['list_of_contacts'])) {
-          $_SESSION['list_of_contacts'] = array();
-        }
+    if(empty($_SESSION['list_of_contacts'])) {
+      $_SESSION['list_of_contacts'] = array();
+    }
 
-        $app = new Silex\Application();
+    $app = new Silex\Application();
 
-        //Twig Path
-        $app->register(new Silex\Provider\TwigServiceProvider(), array(
-          'twig.path' => __DIR__.'/../views'
-        ));
+    //Twig Path
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+      'twig.path' => __DIR__.'/../views'
+    ));
 
-        //Route and Controller
-        $app->get("/", function() use ($app) {
-          $all_contacts = Contact::getAll();
+    //Route and Controller
+    $app->get("/", function() use ($app) {
+      $all_contacts = Contact::getAll();
 
-          return $app['twig']->render('address_book.html.twig', array('contacts' => $all_contacts));
-        });
+      return $app['twig']->render('address_book.html.twig', array('contacts' => $all_contacts));
+    });
 
-        //Contact Post
-        $app->post("/contacts", function() use ($app) {
-          $entry = new Contact($_POST['name'], $_POST['call'], $_POST['address']);
-          $entry->save();
-          return $app['twig']->render('create_contact.html.twig', array('newcontact' => $entry));
-        });
+    //Contact Post
+    $app->post("/contacts", function() use ($app) {
+      $entry = new Contact($_POST['name'], $_POST['call'], $_POST['address']);
+      $entry->save();
+      return $app['twig']->render('create_contact.html.twig', array('newcontact' => $contact));
+    });
 
-        //Delete Method
-        $app->post("/delete_contacts", function() use ($app) {
-          Contact::deleteAll();
-          return $app['twig']->render('delete_contact.html.twig');
-        });
+    //Delete Method
+    $app->post("/delete_contacts", function() use ($app) {
+      Contact::deleteAll();
+      return $app['twig']->render('delete_contact.html.twig');
+    });
 
-        return $app;
+    return $app;
 
  ?>
--- src/Contact.php
+++ src/Contact.php
@@ -1,66 +1,66 @@
 <?php
-        class Contact
+    class Contact
+    {
+      private $contactName;
+      private $contactPhone;
+      private $homeAddress;
+
+      //Constructor
+        function __construct($contactName, $contactPhone, $homeAddress)
         {
-          private $contactName;
-          private $contactPhone;
-          private $homeAddress;
-
-          //Constructor
-            function __construct($contactName, $contactPhone, $homeAddress)
-            {
-              $this->name = $contactName;
-              $this->number = $contactPhone;
-              $this->address = $homeAddress;
-            }
-
-            //Setters
-            function setContactName($new_contact_name)
-            {
-              $this->name = (string) $new_contact_name;
-            }
-
-            function setPhone($new_contact_phone)
-            {
-              $this->number = (float) $new_contact_phone;
-            }
-
-            function setHomeAddress($new_home_address)
-            {
-              $this->address = (string) $new_home_address;
-            }
-
-            //Getters
-            function getContactName()
-            {
-              return $this->name;
-            }
-
-            function getContactPhone()
-            {
-              return $this->number;
-            }
-
-            function getHomeAddress()
-            {
-              return $this->address;
-            }
-
-            //Save Methods
-            function save()
-            {
-              array_push($_SESSION['list_of_contacts'], $this);
-            }
-
-            //Static Methods
-            static function getAll()
-            {
-              return $_SESSION['list_of_contacts'];
-            }
-
-            static function deleteAll()
-            {
-              return $_SESSION['list_of_contacts'] = array();
-            }
+          $this->name = $contactName;
+          $this->number = $contactPhone;
+          $this->address = $homeAddress;
         }
+
+        //Setters
+        function setContactName($new_contact_name)
+        {
+          $this->name = (string) $new_contact_name;
+        }
+
+        function setPhone($new_contact_phone)
+        {
+          $this->number = (float) $new_contact_phone;
+        }
+
+        function setHomeAddress($new_home_address)
+        {
+          $this->address = (string) $new_home_address;
+        }
+
+        //Getters
+        function getContactName()
+        {
+          return $this->name;
+        }
+
+        function getContactPhone()
+        {
+          return $this->number;
+        }
+
+        function getHomeAddress()
+        {
+          return $this->address;
+        }
+
+        //Save Methods
+        function save()
+        {
+          array_push($_SESSION['list_of_contacts'], $this);
+        }
+
+        //Static Methods
+        static function getAll()
+        {
+          return $_SESSION['list_of_contacts'];
+        }
+
+        static function deleteAll()
+        {
+          return $_SESSION['list_of_contacts'] = array();
+        }
+    }
 
  ?>
